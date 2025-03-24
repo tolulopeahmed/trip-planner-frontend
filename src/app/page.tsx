30eff9def4ae75ad6fb647bc12ae6f7a89cac298
Tolulope Ahmed <tolulopeahmed@gmail.com>
2025-03-24 21:10:05 +0100

    Preparing for deployment on Render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,22 +16,23 @@ export default function Home() {
 
       <div className="relative z-10 text-center p-6 max-w-2xl flex flex-col items-center">
         {/* Header (Agrandir + No Line Break) */}
-        <p className="text-5xl md:text-8xl font-bold font-header whitespace-nowrap mb-4">
+        <p className="text-3xl sm:text-5xl md:text-7xl font-bold font-header text-center mb-4">
           Plan Your Perfect Trip
         </p>
 
         {/* Subheader (Italicized) */}
-        <p className="text-lg md:text-xl italic font-section mb-6">
+        <p className="text-sm sm:text-lg md:text-xl italic font-section text-center px-4">
           Enter your trip details and get optimized routes and logs.
         </p>
 
         {/* Centered Button with Gradient Background, Icon & Effects */}
+
         <button
           onClick={() => router.push('/trip-planner')}
-          className="flex items-center gap-3 px-8 py-4 text-lg font-semibold uppercase text-white 
-            bg-gradient-to-r from-purple-600 to-blue-500 
-            hover:from-purple-700 hover:to-blue-600 active:scale-95 
-            transition-all duration-200 ease-in-out rounded-lg shadow-lg"
+          className="flex items-center justify-center gap-2 px-6 py-3 sm:px-8 sm:py-4 text-sm sm:text-lg font-semibold uppercase text-white 
+    bg-gradient-to-r from-purple-600 to-blue-500 
+    hover:from-purple-700 hover:to-blue-600 active:scale-95 
+    transition-all duration-200 ease-in-out rounded-lg shadow-lg w-[90%] sm:w-auto"
         >
           <FaMapMarkedAlt size={30} /> GET STARTED!
         </button>
